e47225f05adac7f88b3d8389bc265b8ce050badf
buley004 <colinbuley@Colins-MBP.domain>
2019-05-21 20:14:54 -0700

    score comparison logic working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,39 @@
 var friendArray = require("../data/friends.js");
 
-module.exports = function(app) {
-  
-  app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+
+  app.get("/api/friends", function (req, res) {
     res.json(friendArray);
   });
 
-  app.post("/api/friends", function(req, res){
-    friendArray.push(req.body);
+  app.post("/api/friends", function (req, res) {
+
+    var friend = req.body;
+    var match;
+    var friendScore = 40;
+
+    //compare incoming friend to all friends in DB
+    for (let i = 0; i < friendArray.length; i++) {
+      var tempScore = 0;
+      //add up difference in scores
+      for (let j = 0; j < friendArray[0].scores.length; j++) {
+        tempScore += Math.abs(friend.scores[j] - friendArray[i].scores[j])
+      }
+      console.log(tempScore);
+      //swap match if difference is lower
+      if (tempScore < friendScore) {
+        friendScore = tempScore;
+        match = friendArray[i];
+      }
+    }
+
+    //console.log(req.body);
+    console.log(friendScore);
+    console.log(match);
+    
+    
+
+    res.json(friendArray);
+    //friendArray.push(req.body);
   })
 };
